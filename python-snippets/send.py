445f47e361fac3dfdd82213f6003ef7d1e329cf9
Aini Hyvärinen <M1468@student.labranet.jamk.fi>
2019-12-15 21:16:15 +0200

    changed some things in send.py

--- python-snippets/send.py
+++ python-snippets/send.py
@@ -9,7 +9,7 @@ def handledata(found_data):
 
     mac = found_data[0]
 
- #   mulla on kaks ruuvitagia niin ne tulee sit erikseen
+ #   mulla on kaks ruuvitagia niin ne tulee sit erikseen tässä
  #
  #   if found_data[0] is '00:00:00:00:00:00':
  #       mac = 1
@@ -18,21 +18,18 @@ def handledata(found_data):
 
     data = found_data[1]
 
-  #  temp = data['temperature']
-
     url = "http://51.83.73.200/iot/sensordata.php"
-    info = { 'user':'oma_käyttis', 'pass':'oma_salasana', 'id':mac, 'temperature': data['temperature'], 'humidity': data['humidity'], 'pressure': data['pressure'] }
-#    r  = requests.post(url=url, data=info)
+    info = {'user': 'oma_käyttis', 'pass': 'oma_salasana', 'deviceid': mac, 'temperature': data['temperature'], 'humidity': data['humidity'], 'pressure': data['pressure']}
 
-    print(info)
- #   print(r.text)
+#    print(info)
 
     global counter
     counter = counter - 1
     if counter <= 0:
         run_flag.running = False
 
+
 mac = '22:22:22:22:22:22' #tähän oma ruuvitag mac-osoite
 
 #macs = ['11:11:11:11:11:11','00:00:00:00:00:00']
-RuuviTagSensor.get_datas(handledata, mac, run_flag)
+RuuviTagSensor.get_datas(handledata, mac, run_flag) # jos 2 > niin tähän mac:n tilalle macs
